b43e6d0ba07db0340e27f2cf7e91b9d342be7acb
limlim1234 <ehrgkrpskf@gmail.com>
2020-12-16 17:49:07 +0900

    lse notice

--- fandemic/src/main/java/com/yedam/fandemic/notice/NoticeController.java
+++ fandemic/src/main/java/com/yedam/fandemic/notice/NoticeController.java
@@ -1,5 +1,19 @@
 package com.yedam.fandemic.notice;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class NoticeController {
+	@RequestMapping(value = "/notice") //주소
+	public ModelAndView notice(HttpServletResponse response) throws IOException {
+		return new ModelAndView("notice/notice_list");
+	
+	}	
 
 }
