c64264230dd7c0e48c08bb559e5116dcd92bdc1d
AO <AO@DESKTOP-JDQNDEU>
2020-12-16 20:00:46 +0900

    warning

--- fandemic/src/main/java/com/yedam/fandemic/mypage/MypageController.java
+++ fandemic/src/main/java/com/yedam/fandemic/mypage/MypageController.java
@@ -1,5 +1,17 @@
 package com.yedam.fandemic.mypage;
 
-public class MypageController {
+import java.io.IOException;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MypageController {
+		@RequestMapping(value = "/mypagemain")
+		public ModelAndView mymain(HttpServletResponse response) throws IOException {
+			return new ModelAndView("mypage/my_main");
+}
 }
