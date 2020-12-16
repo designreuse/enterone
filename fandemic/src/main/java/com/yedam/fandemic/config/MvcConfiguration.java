9172b37e555bb85e2605622dea6a5338ba266f25
Railac <urur700@naver.com>
2020-12-16 15:50:33 +0900

    css

--- fandemic/src/main/java/com/yedam/fandemic/config/MvcConfiguration.java
+++ fandemic/src/main/java/com/yedam/fandemic/config/MvcConfiguration.java
@@ -31,6 +31,7 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter{
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		registry.addResourceHandler("/resourcesAdmin/**").addResourceLocations("/resourcesAdmin/");
 	}
 
 	
