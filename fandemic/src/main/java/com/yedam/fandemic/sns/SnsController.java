7ddadaa5f8d4a6fca3add589786eac6aea78f4c4
AO <AO@DESKTOP-JDQNDEU>
2020-12-16 15:36:31 +0900

    eeee

--- fandemic/src/main/java/com/yedam/fandemic/sns/SnsController.java
+++ fandemic/src/main/java/com/yedam/fandemic/sns/SnsController.java
@@ -1,5 +1,16 @@
 package com.yedam.fandemic.sns;
 
-public class SnsController {
+import java.io.IOException;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
+public class SnsController {
+	@RequestMapping(value = "/sns")
+	public ModelAndView sns(HttpServletResponse response) throws IOException {
+		return new ModelAndView("sns");
+	}
 }
