b093db015e78775344ed295635d13870eda5a216
tripolan94 <qkrwlsrb35@naver.com>
2020-12-30 15:31:00 +0900

    규규 공지사항 페이지네이션 완료!

--- fandemic/src/main/java/com/yedam/fandemic/vo/Cnotice.java
+++ fandemic/src/main/java/com/yedam/fandemic/vo/Cnotice.java
@@ -5,8 +5,9 @@ import lombok.Data;
 
 @Data
 public class Cnotice {
-
-	String cnoc_no;			//게시글번호
+	String cnoc_sub_no;		//실제 화면에 보여주는 게시글번호
+	
+	String cnoc_no;			//DB입력 게시글번호
 	String com_id;			//소속사ID
 	String cnoc_subject;	//게시물말머리
 	String cnoc_title;		//게시물제목
@@ -15,9 +16,8 @@ public class Cnotice {
 	String cnoc_content;	//게시물 내용
 	String cnoc_file;		//게시물 첨부파일
 	String conc_banner;		//홍보용 배너
-	String[] cnoc_nos; //삭제 체크박스들
+	String[] cnoc_nos; 		//삭제 체크박스들
 	
-	String conc_rn;
 	private int cnoc_first; // 페이지네이션
 	private int cnoc_last;
 }
