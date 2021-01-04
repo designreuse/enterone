6541d16ee9e428afbba8dee74ee94b0e53a7c8e1
duavudrkd123 <ypgcute@gmail.com>
2021-01-04 19:19:41 +0900

    zjalt

--- fandemic/src/main/java/com/yedam/fandemic/impl/MypageMapper.java
+++ fandemic/src/main/java/com/yedam/fandemic/impl/MypageMapper.java
@@ -1,5 +1,8 @@
 package com.yedam.fandemic.impl;
 
+import java.util.List;
+
+import com.yedam.fandemic.vo.Letter;
 import com.yedam.fandemic.vo.Member;
 
 public interface MypageMapper {
@@ -7,7 +10,9 @@ public interface MypageMapper {
 	
 //	내정보 수정
 	public int memUpdate (Member member);
-
+	
+	public int SendMail (Letter letter); //쪽지 보내기
+	public List<Letter> selectMail(Letter letter); // 쪽지 조회하기
 
 
 }
