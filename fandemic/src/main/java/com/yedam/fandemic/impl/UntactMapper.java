975f0c59f7661a62cab17f7b04de8ebb9677efca
haez <hae_z@kakao.com>
2021-01-11 17:13:34 +0900

    0111 untact

--- fandemic/src/main/java/com/yedam/fandemic/impl/UntactMapper.java
+++ fandemic/src/main/java/com/yedam/fandemic/impl/UntactMapper.java
@@ -3,7 +3,6 @@ package com.yedam.fandemic.impl;
 import java.util.List;
 
 import com.yedam.fandemic.vo.Goods;
-import com.yedam.fandemic.vo.Untact;
 
 public interface UntactMapper {
 	public List<Goods> untactList();
