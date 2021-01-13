86c0ab998c2ff1b973e22d481f0e279b25021915
limlim1234 <ehrgkrpskf@gmail.com>
2021-01-13 19:32:52 +0900

    se

--- fandemic/src/main/java/com/yedam/fandemic/service/AuditionService.java
+++ fandemic/src/main/java/com/yedam/fandemic/service/AuditionService.java
@@ -1,6 +1,10 @@
 package com.yedam.fandemic.service;
 
+import java.util.List;
+
+
 import com.yedam.fandemic.vo.Activity;
+
 import com.yedam.fandemic.vo.Trainee;
 
 public interface AuditionService {
@@ -8,4 +12,10 @@ public interface AuditionService {
 	public int insertac(Activity activity);
 	//연습생 글 등록
 	public int inserttr(Trainee trainee);
+	//활동지원글 수정
+	public int activityUpdate(Activity activity);
+	//수정을 위한 조회
+	public List<Activity> activityUpdateselect(Activity activity);
+	//활동지원글 삭제
+	public int activityDelete(Activity activity);
 }
