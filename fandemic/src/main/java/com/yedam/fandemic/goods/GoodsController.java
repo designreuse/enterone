758f9982fa185c602efeee2b2702ec15ed966ff0
Dahee <hoihee00@naver.com>
2021-01-13 18:44:18 +0900

    결제

--- fandemic/src/main/java/com/yedam/fandemic/goods/GoodsController.java
+++ fandemic/src/main/java/com/yedam/fandemic/goods/GoodsController.java
@@ -1,7 +1,6 @@
 package com.yedam.fandemic.goods;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -85,45 +83,41 @@ public class GoodsController {
 		model.addAttribute(goMapper.goodsDetail(goods));
 		return new ModelAndView("goods/goods_detail");
 	}
-	
 
 	// Goods 상세화면 - 장바구니 insert
 	@RequestMapping(value = "/goodsDetail", method = RequestMethod.GET)
 	@ResponseBody
 	public void cartInsert(HttpSession session, HttpServletRequest request, Cart cart) throws IOException {
-		Member member = (Member)session.getAttribute("member");	// 세션에 저장해둔 member 불러오기
-		cart.setMem_id(member.getMem_id());						// 불러온 member에서 mem_id만 cart에 담기
+		Member member = (Member) session.getAttribute("member"); // 세션에 저장해둔 member 불러오기
+		cart.setMem_id(member.getMem_id()); // 불러온 member에서 mem_id만 cart에 담기
 		String no = request.getParameter("go_no");
 		cart.setGo_no(no);
 		cart.setCart_qty(request.getParameter("cart_qty"));
 		System.out.println("====================" + no + request.getParameter("cart_qty"));
 		goMapper.cartInsert(cart);
-	 }
- 
-
+	}
 
 	// Goods 상세화면 - 구매정보 insert
-
 
 	// 장바구니 화면 - 목록
 	@RequestMapping(value = "/cart")
 	public ModelAndView cartList(HttpSession session, Model model, Cart cart) throws IOException {
-		Member member = (Member)session.getAttribute("member");	// 세션에 저장해둔 member 불러오기
-		if ( member == null ) { //로그인하지 않은 상태이면 로그인 화면으로 이동
+		Member member = (Member) session.getAttribute("member"); // 세션에 저장해둔 member 불러오기
+		if (member == null) { // 로그인하지 않은 상태이면 로그인 화면으로 이동
 			return new ModelAndView("/login");
 		} else {
-			cart.setMem_id(member.getMem_id());					// 불러온 member에서 mem_id만 cart에 담기
+			cart.setMem_id(member.getMem_id()); // 불러온 member에서 mem_id만 cart에 담기
 			model.addAttribute("cart", goMapper.cartList(cart));
 			return new ModelAndView("goods/goods_cart");
 		}
 	}
-	
+
 	// 장바구니 화면 - 삭제
 	@RequestMapping(value = "/cart/delete", method = RequestMethod.POST)
 	@ResponseBody
 	public boolean cartDel(HttpSession session, HttpServletRequest request, Cart cart) throws IOException {
-		Member member = (Member)session.getAttribute("member");	// 세션에 저장해둔 member 불러오기
-		cart.setMem_id(member.getMem_id());						// 불러온 member에서 mem_id만 cart에 담기
+		Member member = (Member) session.getAttribute("member"); // 세션에 저장해둔 member 불러오기
+		cart.setMem_id(member.getMem_id()); // 불러온 member에서 mem_id만 cart에 담기
 		cart.setCart_no(request.getParameter("cart_no"));
 		goMapper.cartDel(cart);
 		return true;
@@ -133,8 +127,8 @@ public class GoodsController {
 	@RequestMapping(value = "/cart/update", method = RequestMethod.POST)
 	@ResponseBody
 	public boolean cartUpdate(HttpSession session, HttpServletRequest request, Cart cart) throws IOException {
-		Member member = (Member)session.getAttribute("member");	// 세션에 저장해둔 member 불러오기
-		cart.setMem_id(member.getMem_id());						// 불러온 member에서 mem_id만 cart에 담기
+		Member member = (Member) session.getAttribute("member"); // 세션에 저장해둔 member 불러오기
+		cart.setMem_id(member.getMem_id()); // 불러온 member에서 mem_id만 cart에 담기
 		cart.setCart_qty(request.getParameter("cart_qty"));
 		cart.setCart_no(request.getParameter("cart_no"));
 		goMapper.cartUpdate(cart);
@@ -143,53 +137,55 @@ public class GoodsController {
 
 	// 결제 화면 (배송지 입력, 주소 api, 결제 api)
 	@RequestMapping(value = "/goodsBuy")
-	public ModelAndView orderList(HttpSession session, HttpServletRequest request, Model model, Cart cart) throws IOException {
-		Member member = (Member)session.getAttribute("member");	// 세션에 저장해둔 member 불러오기
-		if ( member == null ) { //로그인하지 않은 상태이면 로그인 화면으로 이동
+	public ModelAndView orderList(HttpSession session, HttpServletRequest request, Model model, Cart cart)
+			throws IOException {
+		Member member = (Member) session.getAttribute("member"); // 세션에 저장해둔 member 불러오기
+		if (member == null) { // 로그인하지 않은 상태이면 로그인 화면으로 이동
 			return new ModelAndView("/login");
 		} else {
-			cart.setMem_id(member.getMem_id());					// 불러온 member에서 mem_id만 cart에 담기
+			cart.setMem_id(member.getMem_id()); // 불러온 member에서 mem_id만 cart에 담기
 			model.addAttribute("order", goMapper.orderList(cart));
 			return new ModelAndView("goods/goods_buy");
 		}
 	}
-	
+
 	
 	/*
-	 * https://badstorage.tistory.com/39
-	 * 
-	 * //카트리스트에서 주문하기
-	 * 
-	 * @PostMapping("/cartList") public String order(HttpSession session, OrderVO
-	 * order, @RequestParam(value = "chk[]") List<String> chArr) throws Exception {
-	 * logger.info("order");
-	 * 
-	 * String userId = (String)session.getAttribute("member");
-	 * 
-	 * //주문번호(orderId) 생성을 위한 로직 Calendar cal = Calendar.getInstance(); int year =
-	 * cal.get(Calendar.YEAR); String ym = year + new
-	 * DecimalFormat("00").format(cal.get(Calendar.MONTH) + 1); String ymd = ym +
-	 * new DecimalFormat("00").format(cal.get(Calendar.DATE)); String subNum = "";
-	 * 
-	 * for(int i = 1; i <= 6; i ++) { subNum += (int)(Math.random() * 10); }
-	 * 
-	 * String orderId = ymd + "_" + subNum; //ex) 20200508_373063
-	 * order.setOrderId(orderId); order.setUserId(userId);
-	 * 
-	 * service.orderInfo(order); //주문 테이블 insert
-	 * 
-	 * int cartNum = 0; for(String i : chArr){ cartNum = Integer.parseInt(i);
-	 * System.out.println("cart -> CHK orderList : "+cartNum);
-	 * System.out.println("cart -> orderId orderList : "+orderId);
-	 * service.orderInfoDetails(orderId,cartNum); //주문 상세 테이블 insert
-	 * service.cartDelete(cartNum); //체크되어 들어온 cart번호로 cart table delete }
-	 * 
-	 * 
-	 * return "redirect:/shop/myPage"; }
-	 * 
-	 */
-	
-
+	//카트리스트에서 주문하기 (https://badstorage.tistory.com/39)
+	  
+	@PostMapping("/cartList")
+	public String order(HttpSession session, OrderVO order, @RequestParam(value = "chk[]") List<String> chArr) throws Exception {
+		logger.info("order");	
+		String userId = (String)session.getAttribute("member");
+		
+		//주문번호(orderId) 생성을 위한 로직
+		Calendar cal = Calendar.getInstance();
+		int year = cal.get(Calendar.YEAR);
+		String ym = year + new DecimalFormat("00").format(cal.get(Calendar.MONTH) + 1);
+		String ymd = ym + new DecimalFormat("00").format(cal.get(Calendar.DATE));
+		String subNum = "";
+		
+		for(int i = 1; i <= 6; i ++) {
+			subNum += (int)(Math.random() * 10);
+			}
+		}
+		
+		String orderId = ymd + "_" + subNum; //ex) 20200508_373063
+		order.setOrderId(orderId);
+		order.setUserId(userId);
+		
+		service.orderInfo(order); //주문 테이블 insert
+		
+		int cartNum = 0;
+		for(String i : chArr){
+			cartNum = Integer.parseInt(i);
+			System.out.println("cart -> CHK orderList : "+cartNum);
+			System.out.println("cart -> orderId orderList : "+orderId);
+			service.orderInfoDetails(orderId,cartNum); //주문 상세 테이블 insert
+			service.cartDelete(cartNum); //체크되어 들어온 cart번호로 cart table delete }
+			return "redirect:/shop/myPage";
+		}
+*/
 
 	// 결제 결과 화면 (select)
 	@RequestMapping(value = "/goodsBuyDetail")
