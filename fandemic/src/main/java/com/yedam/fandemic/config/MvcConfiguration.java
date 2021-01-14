2901c213d638baaec229eadd618f8e79d7e63000
Railac <urur700@naver.com>
2021-01-14 19:33:22 +0900

    fu

--- fandemic/src/main/java/com/yedam/fandemic/config/MvcConfiguration.java
+++ fandemic/src/main/java/com/yedam/fandemic/config/MvcConfiguration.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
